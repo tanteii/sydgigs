07179c5a6f0f0cbd17075adda867f5ae5b3e1dc2
Max <maxhuang4242@gmail.com>
2026-04-27 21:19:28 +1000

    Basic scraper implementation. Bandsintown is JS-rendered; Playwright uses to load page and filter by info exposing strcutures due to hashed classnames.

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,7 +1,9 @@
 from scrapers.songkick import scrape_songkick
+from scrapers.bandsintown import scrape_bandsintown
 
 SCRAPERS = {
     "songkick": scrape_songkick,
+    "bandsintown": scrape_bandsintown,
 }
 
-__all__ = ["SCRAPERS", "scrape_songkick"]
+__all__ = ["SCRAPERS", "scrape_songkick", "scrape_bandsintown"]
